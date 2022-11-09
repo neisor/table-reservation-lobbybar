cd6790f8f6ede3091d798b74249b2a609675fc8a
Antonio Raffaele Iannaccone <antonio_iannaccone@swissre.com>
2022-11-09 14:44:07 +0100

    feat: create initial view in views.py

--- table_reservation/reservations/views.py
+++ table_reservation/reservations/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from reservations.forms import CreateReservationForm
+from django.contrib import messages
 
-# Create your views here.
+def create_new_reservation(request):
+    if request.method == "GET":
+        context = {
+            'form': CreateReservationForm
+        }
+        return render(request, 'reservations/create_new_reservation.html', context=context)
+    if request.method == "POST":
+        form = CreateReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Úspešne ste vytvorili novú rezerváciu.")
+            return redirect("/")
+        else:
+            messages.error(request, "Pri validácii dát, ktoré ste zadali, nastala chyba. Skúste to znovu.")
+            context = {
+                "form": form
+            }
+            return render(request, 'reservations/create_new_reservation.html', context=context)
