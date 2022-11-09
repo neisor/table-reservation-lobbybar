bff9758ef42811909fc3a31cd883395f00460bce
Antonio Raffaele Iannaccone <antonio_iannaccone@swissre.com>
2022-11-09 14:43:50 +0100

    feat: create CreateReservationForm in forms.py

---
+++ table_reservation/reservations/forms.py
@@ -0,0 +1,23 @@
+from django.forms import ModelForm, ValidationError
+from django import forms
+from core.models import Reservation
+import datetime
+
+class CreateReservationForm(ModelForm):
+    class Meta:
+        model = Reservation
+        exclude = ('uuid_identificator', 'stav')
+        widgets = {
+            'datum': forms.DateInput(format=('%d.%m.%Y'), attrs={'class':'form-control', 'placeholder':'Vyberte dátum', 'type':'date'}),
+            'cas': forms.TimeInput(format=('%H:%M'), attrs={'class':'form-control', 'placeholder':'Vyberte čas', 'type':'time'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        datum = cleaned_data.get("datum")
+        cas = cleaned_data.get("cas")
+        datum_a_cas = datetime.datetime.combine(datum, cas)
+        now = datetime.datetime.now()
+        if datum_a_cas < now:
+            raise ValidationError('Dátum a čas nemôže byť v minulosti.')
+        
