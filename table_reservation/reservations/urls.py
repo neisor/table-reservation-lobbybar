37a5c95edddefa528712add8a0648b82ca1b22e6
Antonio Raffaele Iannaccone <antonio_iannaccone@swissre.com>
2022-11-09 15:54:05 +0100

    feat: add confirmation url to urls.py

--- table_reservation/reservations/urls.py
+++ table_reservation/reservations/urls.py
@@ -3,4 +3,5 @@ from reservations.views import *
 
 urlpatterns = [
     path('', create_new_reservation, name="create_new_reservation"),
+    path('confirm/<uuid:reservation_uuid4>', confirm_reservation_by_user, name="confirm_reservation_by_user"),
 ]
