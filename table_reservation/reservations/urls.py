2194d615a3d5a351a8b6657476bc2f5b0c76b3f1
Antonio Raffaele Iannaccone <antonio_iannaccone@swissre.com>
2022-11-10 12:48:00 +0100

    feat: add new url for all_reservations view

--- table_reservation/reservations/urls.py
+++ table_reservation/reservations/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from reservations.views import *
 
 urlpatterns = [
+    path('all', all_reservations, name="all_reservations"),
     path('', create_new_reservation, name="create_new_reservation"),
     path('confirm/<uuid:reservation_uuid4>', confirm_reservation_by_user, name="confirm_reservation_by_user"),
 ]
