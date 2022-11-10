dda1dedb5b02167c2de5bce03c36a0ee13e0cc9d
Antonio Raffaele Iannaccone <antonio_iannaccone@swissre.com>
2022-11-10 12:51:23 +0100

    feat: create urls for accepting and declining reservations

--- table_reservation/reservations/urls.py
+++ table_reservation/reservations/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('all', all_reservations, name="all_reservations"),
     path('', create_new_reservation, name="create_new_reservation"),
     path('confirm/<uuid:reservation_uuid4>', confirm_reservation_by_user, name="confirm_reservation_by_user"),
+    path('accept/<uuid:reservation_uuid4>', accept_reservation, name="accept_reservation"),
+    path('decline/<uuid:reservation_uuid4>', decline_reservation, name="decline_reservation"),
 ]
