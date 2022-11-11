f2db9b5ec520011d4e429d23efd45c31324525e0
Antonio Raffaele Iannaccone <antonio_iannaccone@swissre.com>
2022-11-11 14:51:19 +0100

    chore: add comments to CreateReservationForm clean() method

--- table_reservation/reservations/forms.py
+++ table_reservation/reservations/forms.py
@@ -19,12 +19,14 @@ class CreateReservationForm(ModelForm):
         datum = cleaned_data.get("datum")
         cas = cleaned_data.get("cas")
         datum_a_cas_z_formulara = datetime.datetime.combine(datum, cas)
-        now_plus_2_hours = datetime.datetime.now() + datetime.timedelta(hours=2)
+        now_plus_2_hours = datetime.datetime.now() + datetime.timedelta(hours=2)  # Do not allow for a reservation sooner than 2 hours from now
+        # Get casy from PovolenyCas model
         povoleny_cas = PovolenyCas.objects.all().first()
-        if not povoleny_cas:
-            raise ValidationError('Nie je možné zvalidovať čas rezervácie. Je potrebné pridať povolené časy.')
         cas_rezervacii_od = povoleny_cas.cas_rezervacii_od
         cas_rezervacii_do = povoleny_cas.cas_rezervacii_do
+        # Perform validations
+        if not povoleny_cas:
+            raise ValidationError('Nie je možné zvalidovať čas rezervácie. Je potrebné pridať povolené časy.')
         if datum_a_cas_z_formulara < now_plus_2_hours:
             raise ValidationError('Dátum a čas nemôže byť v minulosti a menej ako 2 hodiny pred požadovanou rezerváciou.')
         if cas > cas_rezervacii_do or cas < cas_rezervacii_od:
