d8d75f2ab1043ae0b08ec3fa6f0cee0aba2b33f9
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2022-11-11 19:51:34 +0100

    fix: Fix sending emails and line breaks in html messages

--- table_reservation/reservations/helpers.py
+++ table_reservation/reservations/helpers.py
@@ -26,30 +26,31 @@ def generate_and_send_new_reservation_email_to_customer(request, reservation: Re
     Bez Vášho potvrdenia rezervácie nebude možné Vašu rezerváciu prijať.
     """
     html_message = f"""
-    Dobrý deň,
-    Vašu rezerváciu je potrebné potvrdiť kliknutím na nižšie uvedený odkaz.
+    Dobrý deň,<br/>
+    Vašu rezerváciu je potrebné potvrdiť kliknutím na nižšie uvedený odkaz.<br/><br/>
 
-    <b>Detaily Vašej rezervácie:</b>
-    <b>Počet ľudí:</b> {reservation.pocet_ludi}
-    <b>Dátum:</b> {reservation.datum.strftime("%d.%m.%Y")}
-    <b>Čas:</b> {reservation.cas.strftime("%H:%M")}
-    <b>Meno:</b> {reservation.meno}
-    <b>Priezvisko:</b> {reservation.priezvisko}
-    <b>Tel. č.:</b> {reservation.telefonne_cislo}
-    <b>E-mail:</b> {reservation.email}
+    <b>Detaily Vašej rezervácie:</b><br/>
+    <b>Počet ľudí:</b> {reservation.pocet_ludi}<br/>
+    <b>Dátum:</b> {reservation.datum.strftime("%d.%m.%Y")}<br/>
+    <b>Čas:</b> {reservation.cas.strftime("%H:%M")}<br/>
+    <b>Meno:</b> {reservation.meno}<br/>
+    <b>Priezvisko:</b> {reservation.priezvisko}<br/>
+    <b>Tel. č.:</b> {reservation.telefonne_cislo}<br/>
+    <b>E-mail:</b> {reservation.email}<br/><br/>
 
-    Pre potvrdenie Vašej rezervácie stlačte nasledovný odkaz:
-    <a href="{url_to_confirm_reservation}">{url_to_confirm_reservation}</a>
+    Pre potvrdenie Vašej rezervácie stlačte nasledovný odkaz:<br/>
+    <a href="{url_to_confirm_reservation}">{url_to_confirm_reservation}</a><br/><br/>
 
-    (Skopírujte vyššie uvedený odkaz a vložte ho do prehliadača v prípade nefunkčnosti automatického prekliku.)
+    (Skopírujte vyššie uvedený odkaz a vložte ho do prehliadača v prípade nefunkčnosti automatického prekliku.)<br/><br/>
 
     Bez Vášho potvrdenia rezervácie nebude možné Vašu rezerváciu prijať.
     """
-    recipient = AdminEmail.objects.all().first()
+    admin_email = AdminEmail.objects.all().first()
     send_mail(
-        'El Nacional - Potvrďťe Vašu rezerváciu',
-        plain_text_message,
-        [recipient.email],
+        subject='El Nacional - Potvrďťe Vašu rezerváciu',
+        message=plain_text_message,
+        from_email=admin_email.email,
+        recipient_list=[reservation.email],
         html_message=html_message,
         fail_silently=False,
     )
@@ -81,32 +82,33 @@ def notify_administrator_to_accept_or_decline_reservation(request, reservation: 
     (Skopírujte vyššie uvedený odkaz a vložte ho do prehliadača v prípade nefunkčnosti automatického prekliku.)
     """
     html_message = f"""
-    Dobrý deň,
+    Dobrý deň,<br/><br/>
     
-    Existuje nová rezervácia na potvrdenie alebo zamietnutie.
+    Existuje nová rezervácia na potvrdenie alebo zamietnutie.<br/>
     
-    <b><u>Detaily novej rezervácie:</u></b>
-    <b>Počet ľudí:</b> {reservation.pocet_ludi}
-    <b>Dátum:</b> {reservation.datum.strftime("%d.%m.%Y")}
-    <b>Čas:</b> {reservation.cas.strftime("%H:%M")}
-    <b>Meno:</b> {reservation.meno}
-    <b>Priezvisko:</b> {reservation.priezvisko}
-    <b>Tel. č.:</b> {reservation.telefonne_cislo}
-    <b>E-mail:</b> {reservation.email}
+    <b><u>Detaily novej rezervácie:</u></b><br/>
+    <b>Počet ľudí:</b> {reservation.pocet_ludi}<br/>
+    <b>Dátum:</b> {reservation.datum.strftime("%d.%m.%Y")}<br/>
+    <b>Čas:</b> {reservation.cas.strftime("%H:%M")}<br/>
+    <b>Meno:</b> {reservation.meno}<br/>
+    <b>Priezvisko:</b> {reservation.priezvisko}<br/>
+    <b>Tel. č.:</b> {reservation.telefonne_cislo}<br/>
+    <b>E-mail:</b> {reservation.email}<br/><br/>
 
-    Pre <b>prijatie</b> rezervácie stlačte nasledovný odkaz:
-    <a href="{url_to_accept_reservation}">{url_to_accept_reservation}</a>
+    Pre <b>prijatie</b> rezervácie stlačte nasledovný odkaz:<br/>
+    <a href="{url_to_accept_reservation}">{url_to_accept_reservation}</a><br/><br/>
 
-    Pre <b>zamietnutie</b> rezervácie stlačte nasledovný odkaz:
-    <a href="{url_to_accept_reservation}">{url_to_decline_reservation}</a>
+    Pre <b>zamietnutie</b> rezervácie stlačte nasledovný odkaz:<br/>
+    <a href="{url_to_accept_reservation}">{url_to_decline_reservation}</a><br/><br/>
 
     (Skopírujte vyššie uvedený odkaz a vložte ho do prehliadača v prípade nefunkčnosti automatického prekliku.)
     """
-    recipient = AdminEmail.objects.all().first()
+    admin_email = AdminEmail.objects.all().first()
     send_mail(
-        f'El Nacional - Nová rezervácia - ID: {reservation.id}',
-        plain_text_message,
-        [recipient.email],
+        subject=f'El Nacional - Nová rezervácia - ID: {reservation.id}',
+        message=plain_text_message,
+        from_email=admin_email.email,
+        recipient_list=[admin_email.email],
         html_message=html_message,
         fail_silently=False,
     )
@@ -115,10 +117,10 @@ def notify_administrator_to_accept_or_decline_reservation(request, reservation: 
 
 def notify_customer_about_accepted_or_declined_reservation(reservation: Reservation) -> None:
     if reservation.stav == Reservation.Stavy.POTVRDENA:
-        subject = "'El Nacional - Rezervácia prijatá"
-        message_text = "Vaša rezervácia bola prijatá!"
+        subject = "El Nacional - Rezervácia prijatá"
+        message_text = "Vaša rezervácia bola prijatá! Tešíme sa na Vás."
     else:
-        subject = "'El Nacional - Rezervácia zamietnutá"
+        subject = "El Nacional - Rezervácia zamietnutá"
         message_text = "Vaša rezervácia bola zamietnutá!"
     plain_text_message = f"""
     Dobrý deň,
@@ -135,23 +137,25 @@ def notify_customer_about_accepted_or_declined_reservation(reservation: Reservat
     E-mail: {reservation.email}
     """
     html_message = f"""
-    Dobrý deň,
+    Dobrý deň,<br/><br/>
     
-    {message_text}
+    {message_text}<br/><br/>
     
-    <b><u>Detaily Vašej rezervácie:</u></b>
-    <b>Počet ľudí:</b> {reservation.pocet_ludi}
-    <b>Dátum:</b> {reservation.datum.strftime("%d.%m.%Y")}
-    <b>Čas:</b> {reservation.cas.strftime("%H:%M")}
-    <b>Meno:</b> {reservation.meno}
-    <b>Priezvisko:</b> {reservation.priezvisko}
-    <b>Tel. č.:</b> {reservation.telefonne_cislo}
+    <b><u>Detaily Vašej rezervácie:</u></b><br/>
+    <b>Počet ľudí:</b> {reservation.pocet_ludi}<br/>
+    <b>Dátum:</b> {reservation.datum.strftime("%d.%m.%Y")}<br/>
+    <b>Čas:</b> {reservation.cas.strftime("%H:%M")}<br/>
+    <b>Meno:</b> {reservation.meno}<br/>
+    <b>Priezvisko:</b> {reservation.priezvisko}<br/>
+    <b>Tel. č.:</b> {reservation.telefonne_cislo}<br/>
     <b>E-mail:</b> {reservation.email}
     """
+    admin_email = AdminEmail.objects.all().first()
     send_mail(
-        subject,
-        plain_text_message,
-        [reservation.email],
+        subject=subject,
+        message=plain_text_message,
+        from_email=admin_email.email,
+        recipient_list=[reservation.email],
         html_message=html_message,
         fail_silently=False,
     )
