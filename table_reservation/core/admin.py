f6b60485797c55ee7af5d41b94cf43e304f8491c
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2022-11-12 13:28:45 +0100

    feat: register Aktivita and AdminEmail to admin

--- table_reservation/core/admin.py
+++ table_reservation/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from core.models import Reservation
+from core.models import Reservation, Aktivita, AdminEmail
 
 # Register your models here.
 admin.site.register(Reservation)
+admin.site.register(Aktivita)
+admin.site.register(AdminEmail)
