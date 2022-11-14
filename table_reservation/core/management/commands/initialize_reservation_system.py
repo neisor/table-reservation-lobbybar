d3479df9708f2fd898dad0c19dbd10399ba83cf9
Antonio Raffaele Iannaccone <antonio_iannaccone@swissre.com>
2022-11-14 14:35:45 +0100

    feat: create also adminemail, aktivita and povolenycas in the custom admin script

--- table_reservation/core/management/commands/initialize_reservation_system.py
+++ table_reservation/core/management/commands/initialize_reservation_system.py
@@ -1,5 +1,7 @@
 from django.core.management import BaseCommand, CommandError
 from django.contrib.auth.models import User
+from core.models import PovolenyCas, AdminEmail, Aktivita
+import datetime
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
@@ -11,3 +13,20 @@ class Command(BaseCommand):
         user.is_admin = True
         user.save()
         self.stdout.write('User successfully created!')
+
+        self.stdout.write('Creating PovolenyCas...')
+        povoleny_cas = PovolenyCas.objects.create(
+            cas_rezervacii_od=datetime.time(hour=15, minute=00),
+            cas_rezervacii_do=datetime.time(hour=21, minute=30),
+        )
+        povoleny_cas.save()
+
+        self.stdout.write('Creating AdminEmail')
+        admin_email = AdminEmail.objects.create(email="change-me@change-me.com")
+        admin_email.save()
+
+        self.stdout.write('Creating Aktivita')
+        aktivita = Aktivita.objects.create(nazov="JEDLO")
+        aktivita.save()
+
+        self.stdout.write('DONE!')
