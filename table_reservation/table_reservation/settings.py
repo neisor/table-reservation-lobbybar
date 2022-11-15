2192c9da9245c1374ce8bfa91129bdd46812cbbc
Antonio Raffaele Iannaccone <antonio.iannaccone7@gmail.com>
2022-11-15 08:19:11 +0100

    feat: add if/else in settings.py to set DEBUG = False or True

--- table_reservation/table_reservation/settings.py
+++ table_reservation/table_reservation/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5kv3onega*6znr%vne97*n4qr%r%7bbln(#$767p8ui(pzx=)!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('IN_PRODUCTION'):
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
