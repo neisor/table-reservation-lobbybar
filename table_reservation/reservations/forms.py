cb288d4deafaf2861c505e61fe04f134b363523b
Antonio Raffaele Iannaccone <antonio_iannaccone@swissre.com>
2023-04-06 13:23:21 +0200

    feat: Changed to not allow creating reservations for less than 4 hours before selected time

--- table_reservation/reservations/forms.py
+++ table_reservation/reservations/forms.py
@@ -31,7 +31,7 @@ class CreateReservationForm(ModelForm):
         if type(cas) != datetime.time:
             raise ValidationError('Čas rezervácie ste nezadali v správnom formáte.')  # Needed in case when user does not use time-picker
         datum_a_cas_z_formulara = datetime.datetime.combine(datum, cas)
-        now_plus_2_hours = datetime.datetime.now() + datetime.timedelta(hours=2)  # Do not allow for a reservation sooner than 2 hours from now
+        now_plus_hours = datetime.datetime.now() + datetime.timedelta(hours=4)  # Do not allow for a reservation sooner than 2 hours from now
         # Get casy from PovolenyCas model
         povoleny_cas = PovolenyCas.objects.all().first()
         cas_rezervacii_od = povoleny_cas.cas_rezervacii_od
@@ -39,8 +39,8 @@ class CreateReservationForm(ModelForm):
         # Perform validations
         if not povoleny_cas:
             raise ValidationError('Nie je možné zvalidovať čas rezervácie. Je potrebné pridať povolené časy.')
-        if datum_a_cas_z_formulara < now_plus_2_hours:
-            raise ValidationError('Dátum a čas nemôže byť v minulosti a menej ako 2 hodiny pred požadovanou rezerváciou.')
+        if datum_a_cas_z_formulara < now_plus_hours:
+            raise ValidationError('Dátum a čas ktorý ste zvolili nemôže byť v minulosti a taktiež ani menej ako 4 hodiny pred požadovanou rezerváciou.')
         if cas > cas_rezervacii_do or cas < cas_rezervacii_od:
             raise ValidationError(f'Čas rezervácie musí byť medzi {cas_rezervacii_od.strftime("%H:%M")} až {cas_rezervacii_do.strftime("%H:%M")}.')
         
